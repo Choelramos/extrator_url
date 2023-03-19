9f29527cd039bc4bed08aab30fae2b8ce2483803
Joel <joel4785ramos@gmail.com>
2023-03-19 13:31:10 -0300

    Melhorando as expressões regulares

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,8 +1,10 @@
 import re  # biblioteca ReGex, Regular Expression
 endereco = "Mauricio Gonçalves costa 230, Vila Antonute, 15200-000 José Bonifácio"
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}-{0,1}[0-9]{3}")
 busca = padrao.search(endereco)  # Match
 if busca:
     cep = busca.group()
     print(cep)
+
+
