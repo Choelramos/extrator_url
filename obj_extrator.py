ad3262a1cdf7e2bf72e5fb05f87508f20b575b23
Joel <joel4785ramos@gmail.com>
2023-03-27 22:43:56 -0300

    Fazendo desafio de conversão de valores, obtive ajuda

--- obj_extrator.py
+++ obj_extrator.py
@@ -36,7 +36,7 @@ class ExtratorURL:
         indice_valor = indice_parametro + len(parametro_busca) + 1
         indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
         if indice_e_comercial == -1:
-            valor = self.get_valor_parametros[indice_valor:]
+            valor = self.get_url_parametros()[indice_valor:]
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
 
@@ -52,17 +52,30 @@ class ExtratorURL:
         return self.url == other.url
 
 
-url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
-extrator_url2 = ExtratorURL(url)
 
-print(extrator_url)
-print("O tamanho da URL é: ", len(extrator_url))
-valor_quantidade = extrator_url.get_valor_parametros("quantidade")
-print(valor_quantidade)
-print(id(extrator_url), "\n", id(extrator_url2))
+VALOR_DOLAR = 5.25  # 1 dolar em REAL
+moeda_origem = extrator_url.get_valor_parametros("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametros("moedaDestino")
+quantidade = extrator_url.get_valor_parametros("quantidade")
+
+if moeda_origem == 'real' and moeda_destino == 'dolar':
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print("O valor em R$" + quantidade + " é igual a $" + (str(round(valor_conversao, 2))) + " dólares!")
+elif moeda_origem == 'dolar' and moeda_destino == 'real':
+    valor_concersao = int(quantidade) * VALOR_DOLAR
+    print(("O valor em dolar é $" + quantidade + " que é igual a " + str(valor_concersao) + " reais!"))
+else:
+    print(f'O cambio de {moeda_origem} para {moeda_destino} não está disponível!')
 
 
 
 
+
+#  Meus testes:
+# print(extrator_url)
+# print("O tamanho da URL é: ", len(extrator_url))
+# valor_quantidade = extrator_url.get_valor_parametros("quantidade")
+# print(valor_quantidade)
 # print('__str__' in dir())  # Com o dir podemos ver os métodos da classe
